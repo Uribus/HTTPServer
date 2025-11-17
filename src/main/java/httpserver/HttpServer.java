0074054afb79e1046fcc7b1821b06bcb94f7eedf
Uribus <jdelre01@ucm.es>
2025-11-17 19:55:12 +0100

    Assign static ObjectMapper to Json class variable

--- src/main/java/httpserver/HttpServer.java
+++ src/main/java/httpserver/HttpServer.java
@@ -1,5 +1,10 @@
 package httpserver;
 
+import httpserver.config.ConfigurationManager;
+
 public class HttpServer {
-    
+    public static void main(String[] args) {
+        System.out.println("Setting up server configuration...");
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+    }
 }
