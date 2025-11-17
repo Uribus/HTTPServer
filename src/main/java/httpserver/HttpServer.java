a160128863834058029da182e5cc1e54e4efa030
Uribus <jdelre01@ucm.es>
2025-11-17 20:00:33 +0100

    Set WebRoot path on http.json. Test ConfigurationManager on main with sout.

--- src/main/java/httpserver/HttpServer.java
+++ src/main/java/httpserver/HttpServer.java
@@ -1,10 +1,16 @@
 package httpserver;
 
+import httpserver.config.Configuration;
 import httpserver.config.ConfigurationManager;
 
 public class HttpServer {
     public static void main(String[] args) {
         System.out.println("Setting up server configuration...");
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+        System.out.println("Configuration details\n    Port: " 
+            + conf.getPort() + "\n    WebRoot: " 
+            + conf.getWebroot());
     }
 }
