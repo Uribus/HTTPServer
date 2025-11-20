f33a05f2bd49fb0629bbc1d0288a8b830037223f
Uribus <jdelre01@ucm.es>
2025-11-20 19:43:08 +0100

    Create ServerListenerThread class extending Thread. Move socket logic to ServerListenerThread. Initialize and run ServerListenerThread in a thread in main.

--- src/main/java/httpserver/HttpServer.java
+++ src/main/java/httpserver/HttpServer.java
@@ -8,6 +8,7 @@ import java.net.Socket;
 
 import httpserver.config.Configuration;
 import httpserver.config.ConfigurationManager;
+import httpserver.core.ServerListenerThread;
 
 public class HttpServer {
     public static void main(String[] args) {
@@ -19,37 +20,14 @@ public class HttpServer {
             + conf.getPort() + "\n    WebRoot: " 
             + conf.getWebroot());
 
-        
-        try (
-            // socket setup and listening ready to accept
-            ServerSocket serverSocket = new ServerSocket(conf.getPort());
-            Socket socket = serverSocket.accept();
-            
-            // For reading input
-            InputStream inputStream = socket.getInputStream();
-            // For reading output
-            OutputStream outputStream = socket.getOutputStream();
-        ) {
-        
-            // Ready output
-            String htmlPage = "<html>"
-                                + "<head>"
-                                    + "<title>Simpple HTTP Server</title>" 
-                                + "</head>"
-                                + "<body>"
-                                    + "<h1>Content served by HTTP Server</h1>"
-                                + "</body>"
-                                + "</html>";
-            final String cNf ="\r\n"; // Carriage return and feed line
-            String response = "HTTP/1.1 200 OK"+ cNf // RESPONSE_MESSAGE
-                        + "Content-Length: " + htmlPage.getBytes().length + cNf // HEADER
-                        + cNf + htmlPage + cNf + cNf; // CONTENT
-
-            // Write output
-            outputStream.write(response.getBytes());
-
+        ServerListenerThread serverListenerThread;
+        try {
+            serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
         } catch (IOException e) {
             e.printStackTrace();
+            // TODO handle later
         }
+        
     }
 }
---
+++ src/main/java/httpserver/core/ServerListenerThread.java
@@ -0,0 +1,53 @@
+package httpserver.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerListenerThread extends Thread {
+    private int port;
+    private String webroot;
+    ServerSocket serverSocket;
+
+    public ServerListenerThread(int port, String webroot) throws IOException {
+        this.port = port;
+        this.webroot = webroot;
+        this.serverSocket = new ServerSocket(this.port);
+    }
+
+    @Override
+    public void run() {
+        try (
+            // socket setup and listening ready to accept
+            Socket socket = serverSocket.accept();
+            
+            // For reading input
+            InputStream inputStream = socket.getInputStream();
+            // For reading output
+            OutputStream outputStream = socket.getOutputStream();
+        ) {
+        
+            // Ready output
+            String htmlPage = "<html>"
+                                + "<head>"
+                                    + "<title>Simpple HTTP Server</title>" 
+                                + "</head>"
+                                + "<body>"
+                                    + "<h1>Content served by HTTP Server</h1>"
+                                + "</body>"
+                                + "</html>";
+            final String cNf ="\r\n"; // Carriage return and feed line
+            String response = "HTTP/1.1 200 OK"+ cNf // RESPONSE_MESSAGE
+                        + "Content-Length: " + htmlPage.getBytes().length + cNf // HEADER
+                        + cNf + htmlPage + cNf + cNf; // CONTENT
+
+            // Write output
+            outputStream.write(response.getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
